e7ebeef58117f792f6d3af2330379a2ed3ff8013
Simone Sala <simonesala@me.com>
2020-06-12 11:34:50 +0200

    Coordinate Algolia in create e edit

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <input type="search" id="address-input" placeholder="Where are we going?" />
+    {{-- <input type="search" id="address-input" placeholder="Where are we going?" /> --}}
+    
 @endsection
