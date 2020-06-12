d785eb41e564f233a43691ae1ba5b707b738d884
Francesco Pagano <59645305+frapgn@users.noreply.github.com>
2020-06-12 11:50:19 +0200

    aggiunto {{$message}} agli errori

--- resources/views/user/apartments/create.blade.php
+++ resources/views/user/apartments/create.blade.php
@@ -10,14 +10,14 @@
           <label for="title">Nome appartamento</label>
           <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
             @error('title')
-              <small class="form-text">Errore</small>
+              <small class="form-text">{{$message}}</small>
             @enderror
         </div>
         <div class="form-group">
           <label for="rooms">Numero di camere</label>
           <input type="number" min="0" max="10" id="rooms" name="rooms" value="{{old('rooms')}}">
           @error('rooms')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
 
         </div>
@@ -25,14 +25,14 @@
           <label for="beds">Numero di letti</label>
           <input type="number" min="0" max="20" id="beds" name="beds" value="{{old('beds')}}">
           @error('beds')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
         </div>
         <div class="form-group">
           <label for="baths">Numero di bagni</label>
           <input type="number" min="0" max="10" id="baths" name="baths" value="{{old('baths')}}">
           @error('baths')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
         </div>
         <div class="form-group">
@@ -40,7 +40,7 @@
           <input type="number" class="form-control" id="mq" name="mq" value="{{old('mq')}}">
           {{-- togliere frecce --}}
           @error('mq')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
         </div>
         <div class="form-group">
@@ -48,9 +48,9 @@
           <input type="text" class="form-control address-input" id="address-input" name="address" value="{{old('address')}}">
           <input type="text" class="form-control lat-input" name="latitude">
           <input type="text" class="form-control lng-input" name="longitude">
-          
+
           @error('address')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
         </div>
         <div class="form-group">
@@ -58,7 +58,7 @@
             <input type="file" class="custom-file-input" id="img_path" name="img_path" value="{{old('img_path')}}">
             <label class="custom-file-label" for="img_path">Carica una foto</label>
             @error('img_path')
-              <small class="form-text">Errore</small>
+              <small class="form-text">{{$message}}</small>
             @enderror
           </div>
         </div>
--- resources/views/user/apartments/edit.blade.php
+++ resources/views/user/apartments/edit.blade.php
@@ -10,17 +10,17 @@
           <label for="title">Nome appartamento</label>
           <input type="text" class="form-control" id="title" name="title" value="{{old('title') ?? $apartment->title}}">
             @error('title')
-              <small class="form-text">Errore</small>
+              <small class="form-text">{{$message}}</small>
             @enderror
         </div>
-        
+
         <img src="{{asset('storage/' . $apartment->img_path)}}" class="card-img-top" alt="{{$apartment->title}}">
 
         <div class="form-group">
           <label for="rooms">Numero di camere</label>
           <input type="number" min="0" max="10" id="rooms" name="rooms" value="{{old('rooms') ?? $apartment->rooms}}">
           @error('rooms')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
 
         </div>
@@ -28,14 +28,14 @@
           <label for="beds">Numero di letti</label>
           <input type="number" min="0" max="20" id="beds" name="beds" value="{{old('beds') ?? $apartment->beds}}">
           @error('beds')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
         </div>
         <div class="form-group">
           <label for="baths">Numero di bagni</label>
           <input type="number" min="0" max="10" id="baths" name="baths" value="{{old('baths') ?? $apartment->baths}}">
           @error('baths')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
         </div>
         <div class="form-group">
@@ -43,7 +43,7 @@
           <input type="number" class="form-control" id="mq" name="mq" value="{{old('mq') ?? $apartment->mq}}">
           {{-- togliere frecce --}}
           @error('mq')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
         </div>
         <div class="form-group">
@@ -51,9 +51,9 @@
           <input type="text" class="form-control address-input" id="address-input" name="address" value="{{old('address') ?? $apartment->address}}">
           <input type="text" class="form-control lat-input" name="latitude" value="{{old('latitude') ?? $apartment->latitude}}">
           <input type="text" class="form-control lng-input" name="longitude" value="{{old('longitude') ?? $apartment->longitude}}">
-          
+
           @error('address')
-            <small class="form-text">Errore</small>
+            <small class="form-text">{{$message}}</small>
           @enderror
         </div>
         <div class="form-group">
@@ -61,7 +61,7 @@
             <input type="file" class="custom-file-input" id="img_path" name="img_path" value="{{old('img_path') ?? $apartment->img_path}}">
             <label class="custom-file-label" for="img_path">Carica una nuova foto</label>
             @error('img_path')
-              <small class="form-text">Errore</small>
+              <small class="form-text">{{$message}}</small>
             @enderror
           </div>
         </div>
