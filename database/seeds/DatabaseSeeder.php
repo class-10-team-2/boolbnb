d5f7083b3d43442e359d6e6dff5decfd0b3549ea
Francesco Pagano <59645305+frapgn@users.noreply.github.com>
2020-06-14 18:04:16 +0200

    services seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(ApartmentsTableSeeder::class);
+        $this->call(ServicesTableSeeder::class);
     }
 }
