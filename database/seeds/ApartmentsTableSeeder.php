2b62f528cfde960282fb337c4fbf88ad4c03d1d1
Francesco Pagano <59645305+frapgn@users.noreply.github.com>
2020-06-14 15:23:59 +0200

    apartment and user factory

---
+++ database/seeds/ApartmentsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Apartment;
+
+class ApartmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        factory(Apartment::class, 30)->create();
+    }
+}
