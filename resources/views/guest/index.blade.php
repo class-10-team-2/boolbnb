2c0e4d9217f81476ca67c27ee02012444b1da8e2
Simone <simonesala@me.com>
2020-06-15 15:49:51 +0200

    Algolia instantsearch implementazione

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -3,10 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-12">
-            <input type="search" class="address-input" placeholder="Dove vuoi andare?" />
+        <div class="col-12 instantsearch">
+            {{-- <input type="search" class="address-input" placeholder="Dove vuoi andare?" />
             <input type="text" class="lat-input">   
-            <input type="text" class="lng-input">   
+            <input type="text" class="lng-input">    --}}
+        </div>
+        <div id="hits">
+            
         </div>
     </div>
 </div>
