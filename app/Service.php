50b85702cc331566b255b51fb373638604b27b38
Simone <simonesala@me.com>
2020-06-17 14:38:18 +0200

    Service -> searchable

--- app/Service.php
+++ app/Service.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Service extends Model
 {
+    use Searchable;
     protected $fillable = [
         'name'
     ];
