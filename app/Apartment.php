1b4def0dd07d82fb1afa2c456698d8007af7134b
Francesco Pagano <59645305+frapgn@users.noreply.github.com>
2020-06-18 21:05:15 +0200

    scout extended: test relationship and search

--- app/Apartment.php
+++ app/Apartment.php
@@ -29,4 +29,20 @@ class Apartment extends Model
     {
         return $this->belongsToMany('App\Service');
     }
+
+    // INDICIZZAZIONE DELLE RELAZIONI PER ALGOLIA
+    public function toSearchableArray()
+    {
+        $this->services;
+
+        $array = $this->toArray();
+
+        $array = $this->transform($array);
+
+        $array['first_name'] = $this->user->first_name;
+        // $array['sponsorship_'] = $this->author->email;
+
+        return $array;
+    }
+
 }
--- app/Service.php
+++ app/Service.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Service extends Model
 {
+    use Searchable;
+
     protected $fillable = [
         'name'
     ];
