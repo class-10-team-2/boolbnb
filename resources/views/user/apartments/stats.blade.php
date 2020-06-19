c0437411e2afafeeb92ae94e91334cffb10326e2
lorenzo lulli <lullilore@gmail.com>
2020-06-19 17:13:55 +0200

    chart funzionante

--- resources/views/user/apartments/stats.blade.php
+++ resources/views/user/apartments/stats.blade.php
@@ -7,6 +7,8 @@
     </div>
 
 @endsection
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
 <script>
 $(document).ready(function() {
 
