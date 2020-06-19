2c64210de3d5f01b45084aeae5192a03c8d2b0ac
lorenzo lulli <lullilore@gmail.com>
2020-06-19 17:01:01 +0200

    prova chart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/places.js') }}" defer></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
       <script src="https://js.braintreegateway.com/web/dropin/1.22.1/js/dropin.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 
 
     <!-- Fonts -->
---
+++ resources/views/user/apartments/stats.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="">
+        <canvas id="chart_mensile"></canvas>
+
+    </div>
+
+@endsection
+<script>
+$(document).ready(function() {
+
+    var ctx = $('#chart_mensile');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October','November','December'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, {{$sessions->count()}}, 45, 98, 22, 14, 65, 43]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+});
+
+
+</script>
