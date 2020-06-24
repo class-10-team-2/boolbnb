042ff92b5293608c5635818f0a77228ebbed9678
lorenzo lulli <lullilore@gmail.com>
2020-06-24 17:29:44 +0200

    css user show

--- resources/views/user/apartments/show.blade.php
+++ resources/views/user/apartments/show.blade.php
@@ -1,77 +1,107 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-    <div class="col-12">
-        <div class="card" style="width: 18rem;">
-            {{-- immagine caricata come file --}}
-            <img src="{{asset('storage/' . $apartment->img_path)}}" class="card-img-top" alt="{{$apartment->title}}">
-            {{-- immagine caricata con la factory --}}
-            {{-- <img src="{{asset($apartment->img_path)}}" class="card-img-top" alt="{{$apartment->title}}"> --}}
-            <div class="card-body">
-                <h5 class="card-title">{{$apartment->title}}</h5>
-                <div class="">
-                    <span>{{$apartment->rooms}}</span>
-                </div>
-
-                <div class="">
-                    <span>{{$apartment->baths}}</span>
-                </div>
-
-                <div class="">
-                    <span>{{$apartment->beds}}</span>
-                </div>
-
-                <div class="">
-                    <span>{{$apartment->mq}}</span>
-                </div>
-
-                <div class="">
-                    <p>{{$apartment->address}}</p>
-                </div>
-
-                @foreach ($apartment->services as $service)
-                <div class="">
-                    <p> {{$service->name}}</p>
-                </div>
-                @endforeach
-
-                <a href="{{route('user.apartments.edit', $apartment->id)}}" class="btn btn-primary">Modifica</a>
-                <form class="" action="{{route('user.apartments.destroy', $apartment->id)}}" method="post">
-                    @method('DELETE')
-                    @csrf
-                    <input class="btn btn-danger" type="submit" name="" value="ELIMINA">
-                </form>
-
-            </div>
-        </div>
+<div class="container-fluid">
+    <div class="row row-img">
+        <img class="apt-image" src="{{asset('storage/' .$apartment->img_path)}}" alt="">
     </div>
-</div>
+    <div class="col-12">
+        <div class="row container-margin">
+          <h1 class="apt-title">{{$apartment->title}}</h1>
+        </div>
+        <div class="row container-margin">
+          <h4 class="apt-address"><a href="http://www.google.com/maps/place/{{$apartment->latitude}},{{$apartment->longitude}}" target="_blank"><i class="fas fa-map-marker-alt"></i> {{$apartment->address}}</a></h4>
+        </div>
+        <hr>
+    </div>
+    <div class="row container-margin">
+        <div class="col-8">
+          <div class="apt-info">
+            <span><i class="fas fa-door-open"></i> {{($apartment->rooms > 1) ? $apartment->rooms . ' Camere' : '1 Camera'}}</span>
+            <span><i class="fas fa-bed"></i> {{($apartment->beds > 1) ? $apartment->beds . ' Letti' : '1 Letto'}}</span>
+            <span><i class="fas fa-shower"></i> {{($apartment->baths > 1) ? $apartment->baths . ' Bagni' : '1 Bagno'}}</span>
+            <span><i class="fas fa-home"> </i>{{$apartment->mq}}m<sup>2</sup></span>
+          </div>
+
+
+
+          <h4>Descrizione</h4>
+          <p class="apt-description">
+            {{$apartment->description}}
+          </p>
+
+          <h4>Servizi</h4>
+
+          <div class="apt-services flex-info">
+
+            @foreach ($apartment->services as $service)
+                @if ($service->name == 'Sauna')
+                <span><i class="fas fa-hot-tub"></i> {{$service->name}}</span>
+                @elseif ($service->name == 'Piscina')
+                <span><i class="fas fa-swimming-pool"></i> {{$service->name}}</span>
+                @elseif ($service->name == 'Posto Auto')
+                <span><i class="fas fa-car"></i> {{$service->name}}</span>
+                @elseif ($service->name == 'Portineria')
+                <span><i class="fas fa-concierge-bell"></i> {{$service->name}}</span>
+                @elseif ($service->name == 'Vista Mare')
+                <span><i class="fas fa-water"></i> {{$service->name}}</span>
+                @elseif ($service->name == 'WiFi')
+                <span><i class="fas fa-wifi"></i> {{$service->name}}</span>
+                @endif
+            @endforeach
+          </div>
+          <div class="controls">
+              <a href="{{route('user.apartments.edit', $apartment->id)}}" class="btn btn-edit btn-primary">Modifica</a>
+              <form class="" action="{{route('user.apartments.destroy', $apartment->id)}}" method="post">
+                  @method('DELETE')
+                  @csrf
+                  <input class="btn btn-danger" type="submit" name="" value="Elimina">
+              </form>
+
+
+          </div>
+        </div>
+
+
+
+
+
+
 
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8 col-md-offset-2 payment">
                 {{-- <form class="" action="{{ route('user.apartments.store_sponsoship') }}" method="post"> --}}
                 {{-- <form class="" action="/user/store_sponsoship" method="post"> --}}
                     {{-- @csrf
                     @method('POST') --}}
 
+
                     <input type="hidden" name="id" value="{{$apartment->id}}">
 
 
-                    @foreach ($sponsorship_packs as $sponsorship_pack)
-                        <div class="form-check">
-                            <input type="hidden" name="duration" value="{{$sponsorship_pack->duration}}">
-                            <input class="form-check-input" type="radio" name="sponsorship" value="{{$sponsorship_pack->id}}">
-                            <label>{{$sponsorship_pack->price}} Euro per {{$sponsorship_pack->duration}} ore</label>
-                        </div>
-                    @endforeach
+                    <div class="options">
+                    
+                        <p ><i class="fas fa-chart-line"></i> Highlight your apartment</p>
 
-                    <div id="dropin-container"></div>
-                    <button type="submit" id="submit-button">Conferma metodo di pagamento</button>
+                        @foreach ($sponsorship_packs as $sponsorship_pack)
+                            <div class="form-check">
+                                <input type="hidden" name="duration" value="{{$sponsorship_pack->duration}}">
+                                <input class="form-check-input" type="radio" name="sponsorship" value="{{$sponsorship_pack->id}}">
+                                <label>{{$sponsorship_pack->price}} Euro per {{$sponsorship_pack->duration}} ore</label>
+                            </div>
+                        @endforeach
+
+                    </div>
+
+                    <div class="paypal">
+
+                        <div id="dropin-container"></div>
+                        <button type="submit" id="submit-button">Checkout</button>
+
+                    </div>
                 {{-- </form> --}}
             </div>
         </div>
-
+    </div>
     <script>
         var button = document.querySelector('#submit-button');
         braintree.dropin.create({
