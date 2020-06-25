37350af4851d82ebb9eccf360fa5d0cf4a5152fc
lorenzo lulli <lullilore@gmail.com>
2020-06-25 17:02:01 +0200

    css: messages e index

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
+    <div class="title-margin">
+        <h2>Gestisci i tuoi appartmenti</h2>
+
+    </div>
     <div class="row">
         @if ($apartments->count() == 0)
             <p>Non hai ancora registrato nessun appartamento</p>
             <a class="btn btn-primary" href="{{route('user.apartments.create')}}">Inserisci il tuo appartmento</a>
         @endif
         @foreach ($apartments as $apartment)
-        <div class="col-4">
-            <div class="card" style="width: 18rem;">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 card-container">
+            <div class="card" >
               {{-- <img src="{{asset('storage/' . $apartment->img_path)}}" class="card-img-top" alt="{{$apartment->title}}"> --}}
-              <img src="{{asset($apartment->img_path)}}"" class="card-img-top" alt="{{$apartment->title}}">
+              <img src="{{asset('storage/' . $apartment->img_path)}}"" class="card-img-top img-thumbnail" alt="{{$apartment->title}}">
               <div class="card-body">
-                <h5 class="card-title">{{$apartment->title}}</h5>
+            <h5 class="card-title">{{$apartment->title}}</h5>
                 {{-- <div class="">
                     <span>{{$apartment->rooms}}</span>
 
@@ -29,7 +33,7 @@
                     <span>{{$apartment->mq}}</span>
 
                 </div>
-                
+
                 @foreach ($apartment->services as $service)
 
                     <div class="">
@@ -43,7 +47,7 @@
 
                 </div>
 
-                <a href="{{route('user.apartments.show', $apartment->id)}}" class="btn btn-primary">Gestisci</a>
+                <a href="{{route('user.apartments.show', $apartment->id)}}" class="btn btn-primary btn-show">Gestisci</a>
               </div>
 
             </div>
